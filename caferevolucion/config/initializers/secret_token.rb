87363094a037b8b5a53d345c13df78131a07ac87
Carl W. Handlin <cwallaceh@gmail.com>
2013-07-16 14:39:55 -0500

    Nueva aplicacion

---
+++ caferevolucion/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Caferevolucion::Application.config.secret_key_base = 'f4e4b42538c10a35b4813fc8619c0a5990b0c02da4d8dfa9f2222a3b7fceab757d241d033a61a563b39e954e6967eacf31a9dcec84b92afa2f794627c28a552e'
