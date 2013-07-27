1908689e1e511ca235738247f9d12337da60edda
Carl Wallace <cwallaceh@gmail.com>
2013-07-27 10:48:05 -0500

    Revert "Falta probar posts"
    This reverts commit e76bae58378c3cc594909034fa29103ab3637061.

--- caferevolucion/app/models/post.rb
+++ caferevolucion/app/models/post.rb
@@ -1,6 +1,4 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
 	validates :content, length: { minimum: 120 }
-	validates :user_id, presence: true
-	default_scope -> { order('created_at DESC') }
 end
