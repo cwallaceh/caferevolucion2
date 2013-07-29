3f074e84a2cbf40a1cdd5e3fb8c2db0aa2ddabab
Carl W. Handlin <cwallaceh@gmail.com>
2013-07-29 18:10:34 -0500

    Post funcionando
    ¡IMPORTANTE! es importante recordar los metodos disponibles creados en
    las relación "has_many" y "belongs_to" para sacar el mayo provecho
    recuerda la siguiente URL:
    
    http://ruby.railstutorial.org/chapters/user-microposts#table-association_methods

--- caferevolucion/app/models/user.rb
+++ caferevolucion/app/models/user.rb
@@ -6,5 +6,5 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  has_many :posts
 end
---
+++ caferevolucion/app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :user_id, :title, :category, :body, :created_at, :updated_at
