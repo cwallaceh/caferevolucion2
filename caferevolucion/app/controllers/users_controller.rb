4c9888f0d8d7b7a29cd93f520030b5968a25a581
Carl Wallace <cwallaceh@gmail.com>
2013-08-05 21:49:46 -0500

    Truena en _user.html.erb

--- caferevolucion/app/controllers/users_controller.rb
+++ caferevolucion/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
 before_filter :authenticate_user!
+#load_and_authorize_resource :only => :index
 
   def index
   	@users = User.all
--- caferevolucion/app/models/user.rb
+++ caferevolucion/app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true
   validates :username, :uniqueness => {:case_sensitive => false}
+  validates :email, :uniqueness => {:case_sensitive => false}
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
