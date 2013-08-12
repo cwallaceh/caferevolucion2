34efdca5faab14b9677cbd6ecb21efd8d6262d00
Carl Wallace <cwallaceh@gmail.com>
2013-08-12 01:39:13 -0500

    Empezando fase no se cual FOLLOW

--- caferevolucion/app/models/ability.rb
+++ caferevolucion/app/models/ability.rb
@@ -3,12 +3,14 @@ class Ability
 
   def initialize(user)
         user ||= User.new # guest user (not logged in)
-        if user.has_role? :Admin
+        if user.has_role? :admin
             can :manage, :all
-        end
-
-        if user.has_role? :Author
+        else 
             can :read, :all
+        end
+        #Checar si se puede usar ELSE IF
+        if user.has_role? :author
+            can :write, :all
         end
   end
 end
