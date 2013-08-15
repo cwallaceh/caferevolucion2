9345e2302cc7046c94ecb4f4f6ad4c75cf6b5a4d
Carl W. Handlin <cwallaceh@gmail.com>
2013-08-15 15:01:55 -0500

    Definir bien las condiciones CAN
    problema con los posts, arreglar con load and authorize

--- caferevolucion/app/models/ability.rb
+++ caferevolucion/app/models/ability.rb
@@ -7,7 +7,11 @@ class Ability
         if user.has_role? :admin
             can :manage, :all
         elsif user.has_role? :author
-            can :read, :all
+            can :manage, :all
+            #can :create, Post
+            #can :update, Post do |post|
+            #	post.try(:user) == user
+            #end
         end
         
   end
